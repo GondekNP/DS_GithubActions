21925436ba14eae9ed9c7724ecb9f74b9995f78e
GondekNP <gondeknp@gmail.com>
2023-07-20 07:34:21 -0700

    first, refactor to be unit-testable

--- monte_carlo_pi.py
+++ monte_carlo_pi.py
@@ -1,11 +1,23 @@
 ## function that uses the monte carlo method to estimate pi
 import random
 
+
+def random_point():
+    """Generate a random point in the unit square (0,0) to (1,1)."""
+    x = random.uniform(0, 1)
+    y = random.uniform(0, 1)
+    return x, y
+
+
+def in_circle(x, y):
+    """Check whether the point (x, y) lies within the unit circle."""
+    distance = x**2 + y**2
+    return distance <= 1
+
+
 def estimate_pi(n):
     """
     Estimate the value of pi using the Monte Carlo method.
-
-    This function generates n random points in the unit square (0, 0) to (1,1) and determines the proportion that lies within the unit circle. It then multiplies this proportion by 4 to estimate pi.
 
     Args:
         n (int): The number of random points to generate.
@@ -16,12 +28,11 @@ def estimate_pi(n):
     num_points_circle = 0
     num_points_total = 0
     for _ in range(n):
-        x = random.uniform(0, 1)
-        y = random.uniform(0, 1)
-        distance = x**2 + y**2
-        if distance <= 1:
+        x, y = random_point()
+        if in_circle(x, y):
             num_points_circle += 1
         num_points_total += 1
+
     return 4 * num_points_circle / num_points_total
 
 if __name__ == "__main__":
