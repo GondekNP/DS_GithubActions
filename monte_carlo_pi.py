07fdf743dd84806103444f2bcae66b2dd0957de6
GondekNP <gondeknp@gmail.com>
2023-07-20 06:48:19 -0700

    woah smells way better now

--- monte_carlo_pi.py
+++ monte_carlo_pi.py
@@ -1,7 +1,7 @@
 import random
 
 
-def estimatePI(n):
+def estimate_pi(n):
     """
     Estimate the value of pi using the Monte Carlo method.
 
@@ -15,17 +15,18 @@ def estimatePI(n):
     Returns:
         float: The estimated value of pi.
     """
-    numPointsCircle = 0
+    num_points_circle = 0
     num_points_total = 0
 
     for _ in range(n):
-        x = random.uniform(0, 1) ; y = random.uniform(0, 1)  # smelly semicolon
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)  # smelly semicolon
         distance = x**2 + y**2
         if distance <= 1:
-            numPointsCircle += 1
+            num_points_circle += 1
         num_points_total += 1
-    return 4 * numPointsCircle / num_points_total  # smelly lack of spacing
+    return 4 * num_points_circle / num_points_total  # smelly lack of spacing
 
 
 if __name__ == "__main__":
-    print(estimatePI(1000000))
+    print(estimate_pi(1000000))
